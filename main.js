a865e24a6af21a8e4607abfb284b68587e3d5a2f
Caio Leme <caioacleme@gmail.com>
2022-12-19 17:13:24 -0300

    Updated main.js
    Updated main.js: start consuming GitHub API for profile, project and stats update

--- main.js
+++ main.js
@@ -2,5 +2,15 @@ const projeto1 = document.getElementById('projeto1')
 
 function updateProjects() {
   console.log('rodando o JS')
-  //let url = URL da API do github
+  let url = `https://api.github.com/users/caiolemedev`
+
+  axios
+    .get(url)
+    .then(res => {
+      const data = res.data
+      console.log(data)
+    })
+    .catch(error => console.log('falhou!'))
 }
+
+updateProjects()
