f07a8a86f05217fcc0860c2117410bb26d3ce662
Caio Leme <caioacleme@gmail.com>
2022-12-20 19:39:31 -0300

    Update main.js
    Update main.js: use GitHub API to change user photo, name and repos name. Starting to construct autoupdate portfolio function.

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
-const projeto1 = document.getElementById('projeto1')
+const perfil = document.getElementById('perfil')
+const userName = perfil.querySelector('h1')
+const userPhoto = perfil.querySelector('img')
+const project1 = document.getElementById('project1')
+const project1Name = project1.querySelector('h3')
 
-function updateProjects() {
+const user = 'caiolemedev'
+
+function updatePortfolio() {
   console.log('rodando o JS')
-  let url = `https://api.github.com/users/caiolemedev`
+  let url = `https://api.github.com/users/${user}`
 
   axios
     .get(url)
     .then(res => {
       const data = res.data
       console.log(data)
+      setUserInfo(data)
+
+      axios
+        .get(data.repos_url)
+        .then(res => {
+          const repos = res.data
+          console.log(repos)
+          setProjectInfo(repos[1])
+        })
+        .catch(error => {
+          console.log(error)
+        })
     })
-    .catch(error => console.log('falhou!'))
+    .catch(error => {
+      console.log(error)
+    })
 }
 
-updateProjects()
+function setUserInfo(data) {
+  userName.textContent = `${data.name}`
+  userPhoto.src = `${data.avatar_url}`
+}
+
+function setProjectInfo(repo) {
+  project1Name.textContent = `${repo.name}`
+}
+
+updatePortfolio()
